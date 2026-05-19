ae4270a359218fb8020a6bf8213cb5d36606c06e
opwenam <opwenam@localhost>
2026-05-18 20:19:37 -0400

    project initiation
    # Conflicts:
    #	LICENSE

---
+++ src/client/java/com/opwenam/yawpplayerclaim/client/YawpPlayerClaimClient.java
@@ -0,0 +1,10 @@
+package com.opwenam.yawpplayerclaim.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YawpPlayerClaimClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
